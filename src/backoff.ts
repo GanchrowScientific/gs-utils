5729f3e80526833c068d3c0fbe98b557b4d9f019
Andy Ganchrow <andy@ganchrow.com>
2016-05-06 09:24:35 -0700

    Exponential backoff class and typeOf decorator
    Change-Id: Ia5c9b6e961fbb8c6754250a12779ab8f2962b0db

---
+++ src/backoff.ts
@@ -0,0 +1,70 @@
+/* Copyright © 2016 Ganchrow Scientific, SA all rights reserved */
+
+'use strict';
+
+
+const DEFAULT_RETRIES = 10;
+const DEFAULT_INTERVAL = 100;
+const DEFAULT_TRUNCATE = Infinity;
+
+interface BackoffOpts {
+  retries?: number;
+  baseInterval?: number;
+  truncate?: number;
+}
+
+enum BackoffState {
+  STOPPED = 0,
+  FIRING
+};
+
+export class Backoff {
+  private registeredCBs = [];
+  private registeredEnds = [];
+  private timeout: any;
+  private retryCount = 0;
+  private retries: number;
+  private baseInterval: number;
+  private truncate: number;
+  private state = BackoffState.STOPPED;
+
+  constructor(backoffOpts = {}) {
+    this.retries = (<BackoffOpts>backoffOpts).retries || DEFAULT_RETRIES;
+    this.baseInterval = (<BackoffOpts>backoffOpts).baseInterval || DEFAULT_INTERVAL;
+    this.truncate = (<BackoffOpts>backoffOpts).truncate || DEFAULT_TRUNCATE;
+  }
+
+  public register(cb: (...args: any[]) => void) {
+    this.registeredCBs.push(cb);
+  }
+
+  public onEnd(cb: () => void) {
+    this.registeredEnds.push(cb);
+  }
+
+  public fire(...args: any[]) {
+    if (this.retryCount >= this.retries) {
+      this.reset();
+      process.nextTick(() => {
+        this.registeredEnds.forEach(cb => cb());
+      });
+    } else if (this.state === BackoffState.STOPPED) {
+      this.state = BackoffState.FIRING;
+      this.timeout = setTimeout(() => {
+        this.state = BackoffState.STOPPED;
+        this.registeredCBs.forEach(cb => cb(...args));
+      }, this.calculateTime());
+      this.retryCount++;
+    }
+  }
+
+  public reset() {
+    this.retryCount = 0;
+    this.state = BackoffState.STOPPED;
+    clearTimeout(this.timeout);
+  }
+
+  private calculateTime(): number {
+    return Math.min(this.truncate, this.baseInterval * Math.exp(this.retryCount * this.baseInterval / 1000));
+  }
+}
---
+++ test/backoff.test.ts
@@ -0,0 +1,143 @@
+/* Copyright © 2016 Ganchrow Scientific, SA all rights reserved */
+'use strict';
+
+/// <reference path="../typings/nodeunit/nodeunit.d.ts"/>
+
+// include this line to fix stack traces
+import 'source-map-support/register';
+
+import {Backoff} from '../src/backoff';
+import * as sinon from 'sinon';
+
+const PRIVATE_CALC_TIME = 'calculateTime';
+
+module.exports = {
+  setUp: function(callback) {
+    this.clock = sinon.useFakeTimers();
+    callback();
+  },
+
+  testFire: function(test: nodeunit.Test) {
+    let backoff = new Backoff();
+    backoff.register(() => {
+      backoff.reset();
+      test.ok(true);
+      test.done();
+    });
+    backoff.fire();
+    this.clock.tick(1000);
+  },
+
+  testBackoffTime: function(test: nodeunit.Test) {
+    let backoff = new Backoff();
+    let time = getNow();
+    backoff.register(() => {
+      let now = getNow();
+      backoff.reset();
+      test.equals(now, time + 100);
+      test.done();
+    });
+    backoff.fire();
+    this.clock.tick(backoff[PRIVATE_CALC_TIME]());
+  },
+
+  testBackoffParamTime: function(test: nodeunit.Test) {
+    let interval = 200;
+    let backoff = new Backoff({baseInterval: interval});
+    let time = getNow();
+    backoff.register(() => {
+      let now = getNow();
+      backoff.reset();
+      test.equals(now, time + interval);
+      test.done();
+    });
+    backoff.fire();
+    this.clock.tick(backoff[PRIVATE_CALC_TIME]());
+  },
+
+  testBackoffSecondTime: function(test: nodeunit.Test) {
+    let backoff = new Backoff();
+    let count = 0;
+    let time;
+    backoff.register(() => {
+      count++;
+      if (count === 2) {
+        let now = getNow();
+        backoff.reset();
+        test.equals(Math.trunc(now), Math.trunc(time + 100 * Math.exp(1 / 10)));
+        test.done();
+      } else {
+        time = getNow();
+        backoff.fire();
+        this.clock.tick(backoff[PRIVATE_CALC_TIME]());
+      }
+    });
+    backoff.fire();
+    this.clock.tick(backoff[PRIVATE_CALC_TIME]());
+  },
+
+  testBackoffThirdTime: function(test: nodeunit.Test) {
+    let backoff = new Backoff();
+    let count = 0;
+    let time;
+    backoff.register(() => {
+      count++;
+      if (count === 3) {
+        let now = getNow();
+        backoff.reset();
+        test.equals(Math.trunc(now), Math.trunc(time + 100 * Math.exp(2 / 10)));
+        test.done();
+      } else {
+        time = getNow();
+        backoff.fire();
+        this.clock.tick(backoff[PRIVATE_CALC_TIME]());
+      }
+    });
+    backoff.fire();
+    this.clock.tick(backoff[PRIVATE_CALC_TIME]());
+  },
+
+  testBackoffEnds: function(test: nodeunit.Test) {
+    let backoff = new Backoff();
+    let count = 0;
+    backoff.register(() => {
+      count++;
+      backoff.fire();
+      this.clock.tick(1000);
+    });
+    backoff.onEnd(() => {
+      test.ok(true);
+      test.equals(count, 10);
+      test.done();
+    });
+    backoff.fire();
+    this.clock.tick(1000);
+  },
+
+  testBackoffEndsRetryParam: function(test: nodeunit.Test) {
+    let backoff = new Backoff({retries: 2});
+    let count = 0;
+    backoff.register(() => {
+      count++;
+      backoff.fire();
+      this.clock.tick(1000);
+    });
+    backoff.onEnd(() => {
+      test.ok(true);
+      test.equals(count, 2);
+      test.done();
+    });
+    backoff.fire();
+    this.clock.tick(1000);
+  },
+
+  tearDown: function(callback) {
+    this.clock.restore();
+    callback();
+  }
+};
+
+function getNow() {
+  return Date.now();
+};
+
