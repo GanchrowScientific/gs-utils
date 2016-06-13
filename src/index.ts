097eaa5132139570543e9eefb50f454751af2dea
Andy Ganchrow <andy@ganchrow.com>
2016-06-13 15:56:54 -0700

    Package properly so require('gs-utils') gives you access to all the libraries
      * still compatible with importing libs directly
    
    Change-Id: I1b9d4c5116648c6e26b1b873dd810bdf7e4b5a24

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts"/>
+/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
 'use strict';
+
+import * as utils from './utilities';
+import * as gsLogger from './gsLogger';
+import {Chunker} from './chunker';
+import {ByteSizedChunker} from './byteSizedChunker';
+import {include} from './decorators/include';
+import {classify} from './decorators/classify';
+import {lowercaseArguments} from './decorators/lowercaseArguments';
+import {precondition} from './decorators/precondition';
+import {setOnce} from './decorators/setOnce';
+import {Backoff} from './backoff';
+
+export var gsutils = Object.defineProperties(utils, {
+  Chunker: { value: Chunker },
+  ByteSizedChunker: { value: ByteSizedChunker },
+  decorators: { value: Object.defineProperties({}, {
+    include: { value: include },
+    classify: { value: classify },
+    lowercaseArguments: { value: lowercaseArguments },
+    setOnce: { value: setOnce },
+    precondition: { value: precondition }
+  }) },
+  Backoff: { value: Backoff },
+  gsLogger: { value: gsLogger }
+});
