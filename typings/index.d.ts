4babff7bf30ed799ebac6b2aa1f4020102c9c326
Andrew Eisenberg <andrew@ganchrow.com>
2016-06-28 13:05:36 -0600

    Installs nodemail as an npm dependency
    For sending emails on fatal errors.
    
    Change-Id: Ifcb881522715c4ef8c40405140e983f87a92de5d

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="custom/bufferpack.d.ts" />
 /// <reference path="globals/chalk/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/nodemailer/index.d.ts" />
 /// <reference path="globals/nodeunit/index.d.ts" />
 /// <reference path="globals/sinon/index.d.ts" />
