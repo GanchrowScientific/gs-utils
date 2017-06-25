e4a410305c5e25f6f7fe77cef0ef0f5c673db6de
Andy Ganchrow <andy@ganchrow.com>
2017-06-25 10:41:49 -0700

    Overwrite redis.createClient method to resolve unix socket connections
      * Intelligently uses either host or path depending on target machine location
      * Overwrite createClient method 'redis' library
      * 'redis' library currently either uses host or path when provided
      * Fails when path is not accessible, rather than falling back to host
    
    Change-Id: I58b0314116ad2b9daa31fff13f97b446aa47bcb8
    Reviewed-on: http://gerrit.ganchrow.com:8080/4042
    Tested-by: Gerrit Code Review on Ashley <bot@ganchrow.com>
    Reviewed-by: Andy Addington <andy.addington@ganchrow.com>

---
+++ src/redisUnixPathResolver.ts
@@ -0,0 +1,34 @@
+/* Copyright © 2017 Ganchrow Scientific, SA all rights reserved */
+'use strict';
+
+import * as redis from 'redis';
+
+import {isObject, dup} from './utilities';
+import {isRemoteHost} from './hostName';
+
+interface BasicRedisConfig {
+  host: string;
+  port: number;
+  path?: string;
+}
+
+export const install = () => {
+  let createClient = redis.createClient.bind(redis);
+  Object.defineProperty(redis, 'createClient', {
+    value: (config: BasicRedisConfig, ...args: any[]) => {
+      if (isObject(config)) {
+        let redisConfig = dup(config);
+        if (redisConfig.path) {
+          if (isRemoteHost(redisConfig.host)) {
+            delete redisConfig.path;
+          } else {
+            delete redisConfig.port;
+            delete redisConfig.host;
+          }
+        }
+        return createClient(redisConfig, ...args);
+      }
+      return createClient(config, ...args);
+    }
+  });
+};
