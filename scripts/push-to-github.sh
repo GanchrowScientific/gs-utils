87d6151373a172a676c77e4c589ab36e034aa499
Andrew Eisenberg <andrew@ganchrow.com>
2017-11-29 11:27:30 -0600

    Changes mechanism for getting the package name
    Change-Id: Ia35e2ecd56db9b549b44ec55a1d09894693483e5
    Reviewed-on: http://gerrit.ganchrow.com:8080/5260
    Tested-by: Gerrit Code Review on Ashley <bot@ganchrow.com>
    Reviewed-by: Andrew Eisenberg <andrew.eisenberg@ganchrow.com>

--- scripts/push-to-github.sh
+++ scripts/push-to-github.sh
@@ -2,7 +2,8 @@
 
 PROJECT_PATH="$(dirname $0)/../"
 
-VERSION=$(${PROJECT_PATH}scripts/get-version $PROJECT_PATH)
+VERSION=$(${PROJECT_PATH}scripts/get-package-property $PROJECT_PATH version)
+PROJECT_NAME=$(${PROJECT_PATH}scripts/get-package-property $PROJECT_PATH name)
 
 echo Creating tag $VERSION
 git tag -f $VERSION
