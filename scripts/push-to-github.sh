1c46626648b3e3273845cc3d11896fdf3ac92a05
Andrew Eisenberg <andrew@ganchrow.com>
2017-11-29 10:23:02 -0600

    Adds public registry and fixes git push command
    Change-Id: Iea65eb4f5b802d7038b43eec4b7f595dec0291fb
    Reviewed-on: http://gerrit.ganchrow.com:8080/5250
    Tested-by: Gerrit Code Review on Ashley <bot@ganchrow.com>
    Reviewed-by: Andrew Eisenberg <andrew.eisenberg@ganchrow.com>

--- scripts/push-to-github.sh
+++ scripts/push-to-github.sh
@@ -8,4 +8,4 @@ echo Creating tag $VERSION
 git tag -f $VERSION
 
 echo Pushing to github
-git push git@github.com:GanchrowScientific/gs-utils.git master $VERSION
+git push -f git@github.com:GanchrowScientific/gs-utils.git +HEAD:master $VERSION
