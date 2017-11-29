bdf382eadf75c7f6280b0cc9515516e9b10bdc4a
Andrew Eisenberg <andrew@ganchrow.com>
2017-11-29 11:28:29 -0600

    Changes mechanism for getting the package name
    Change-Id: I703683c08a7574149c09739e65530432cf9b0854
    Reviewed-on: http://gerrit.ganchrow.com:8080/5259
    Tested-by: Gerrit Code Review on Ashley <bot@ganchrow.com>
    Reviewed-by: Andrew Eisenberg <andrew.eisenberg@ganchrow.com>

--- scripts/push-to-github.sh
+++ scripts/push-to-github.sh
@@ -2,10 +2,11 @@
 
 PROJECT_PATH="$(dirname $0)/../"
 
-VERSION=$(${PROJECT_PATH}scripts/get-version $PROJECT_PATH)
+VERSION=$(${PROJECT_PATH}scripts/get-package-property $PROJECT_PATH version)
+PROJECT_NAME=$(${PROJECT_PATH}scripts/get-package-property $PROJECT_PATH name)
 
 echo Creating tag $VERSION
 git tag -f $VERSION
 
 echo Pushing to github
-git push -f git@github.com:GanchrowScientific/gs-utils.git +HEAD:master $VERSION
+git push -f git@github.com:GanchrowScientific/${PROJECT_NAME}.git +HEAD:master $VERSION
