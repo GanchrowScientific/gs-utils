a2f5cc7143be87ac2328e6df9efa5e633bc060c2
Andy Ganchrow <andy.addington@ganchrow.com>
2018-03-06 10:50:18 -0800

    Add generic typing to wrapAsync
    Change-Id: I1afb5617a0043f7ec29644b7eaf5ad4c85f753a0
    Reviewed-on: http://gerrit.ganchrow.com:8080/6374
    Tested-by: Gerrit Code Review on Ashley <bot@ganchrow.com>
    Reviewed-by: Andy Addington <andy.addington@ganchrow.com>

--- src/wrapAsync.ts
+++ src/wrapAsync.ts
@@ -1,7 +1,7 @@
-/* Copyright © 2017 Ganchrow Scientific, SA all rights reserved */
+/* Copyright © 2017-2018 Ganchrow Scientific, SA all rights reserved */
 'use strict';
 
-export function wrapAsync(func: Function, ...args: any[]): Promise<any> {
+export function wrapAsync<T>(func: Function, ...args: any[]): Promise<T> {
   return new Promise((resolve, reject) => {
     func(...(args.concat((err, res) => {
       if (err) {
