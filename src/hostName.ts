b0b077c4e189d75e324249fc5ef51b290e95ee74
Brian <brian.alonso@ganchrow.com>
2021-02-09 14:28:31 -0300

    Add async isLocalHost function that uses dns lookup
    Change-Id: I50aeaeea20d1bbf470802a536a54cf87506c8afe
    Reviewed-on: http://gerrit.ganchrow.com:8080/c/gs-utils/+/18090
    Tested-by: Ganchrow Bot <bot@ganchrow.com>
    Reviewed-by: Andy Addington <andy.addington@ganchrow.com>
    Reviewed-by: brian <brian.alonso@ganchrow.com>

--- src/hostName.ts
+++ src/hostName.ts
@@ -1,18 +1,31 @@
-/* Copyright © 2016-2018 Ganchrow Scientific, SA all rights reserved */
+/* Copyright © 2016-2021 Ganchrow Scientific, SA all rights reserved */
 
 'use strict';
 
 import * as os from 'os';
+import { promises } from 'dns';
+const { lookup } = promises;
 
 export function getNetworkIP(): string {
   try {
-    let ifaces = os.networkInterfaces();
-    return Object.keys(ifaces)
-      .map(i => ifaces[i].filter(x => x.family === 'IPv4' && !x.internal)[0])
-      .filter(f => !!f)[0].address;
+    return getNetworkIPs()[0];
   } catch (e) {
     // assume network is turned off
     return '';
+  }
+}
+
+export function getNetworkIPs(): string[] {
+  try {
+    let ifaces = os.networkInterfaces();
+    return Object.keys(ifaces)
+      .map(i => ifaces[i]
+      .filter(x => x.family === 'IPv4' && !x.internal)[0])
+      .filter(f => !!f)
+      .map(iface => iface.address);
+  } catch (e) {
+    // assume network is turned off
+    return [''];
   }
 }
 
@@ -21,9 +34,20 @@ function isExactMatchWrap(item: any): (otherItem: any) => boolean {
 }
 
 function possibleLocalHostNames(): any[] {
-  return ['localhost', '127.0.0.1', os.hostname(), getNetworkIP()];
+  return ['localhost', '127.0.0.1', os.hostname(), ...getNetworkIPs()];
 }
 
 export function isRemoteHost(host: any): boolean {
   return !(!host || possibleLocalHostNames().some(isExactMatchWrap(host)));
+}
+
+export async function isLocalHost(host: string): Promise<boolean> {
+  let ip;
+  try {
+    ip = await lookup(host);
+  } catch (error) {
+    // the name coudn't be resolved so it will be ignored
+  }
+  return ip?.address ? possibleLocalHostNames().some(isExactMatchWrap(ip.address)) :
+    !isRemoteHost(host);
 }
--- test/hostName.test.ts
+++ test/hostName.test.ts
@@ -1,41 +1,90 @@
-/* Copyright © 2016-2018 Ganchrow Scientific, SA all rights reserved */
+/* Copyright © 2016-2021 Ganchrow Scientific, SA all rights reserved */
 'use strict';
 
 // include this line to fix stack traces
 import 'source-map-support/register';
-import * as os from 'os';
+import * as pq from 'proxyquire';
+import * as sinon from 'sinon';
 
 import 'jasmine';
 
-import {testWrapper} from '../src/jasmineTestWrapper';
+import { testWrapper } from '../src/jasmineTestWrapper';
 
 const test = testWrapper.init(expect);
+const proxyquire = pq.noPreserveCache();
 
-import {isRemoteHost, getNetworkIP} from '../src/hostName';
+describe('hostname', () => {
 
-describe('IsRemoteHost', () => {
-  it('should return isRemoteHost', () => {
-    test.ok(!isRemoteHost(''));
-    test.ok(!isRemoteHost(null));
-    test.ok(!isRemoteHost(false));
-    test.ok(!isRemoteHost(undefined));
-    test.ok(!isRemoteHost(0));
-    test.ok(!isRemoteHost('localhost'));
-    test.ok(!isRemoteHost('127.0.0.1'));
-    test.ok(isRemoteHost('notlocal'));
-    test.ok(isRemoteHost('1.2.3.4'));
+  describe('IsRemoteHost', () => {
+    it('should return isRemoteHost', () => {
+      const { isRemoteHost } = createMocks(sinon.stub());
+      test.ok(!isRemoteHost(''));
+      test.ok(!isRemoteHost(null));
+      test.ok(!isRemoteHost(false));
+      test.ok(!isRemoteHost(undefined));
+      test.ok(!isRemoteHost(0));
+      test.ok(!isRemoteHost('localhost'));
+      test.ok(!isRemoteHost('127.0.0.1'));
+      test.ok(isRemoteHost('notlocal'));
+      test.ok(isRemoteHost('1.2.3.4'));
+    });
+
+    it('should get network ip no network', () => {
+      const { getNetworkIP } = createMocks(sinon.stub().throwsException());
+      try {
+        test.strictEqual(getNetworkIP(), '');
+      } catch (e) {
+        test.ok(false, e.message);
+      }
+    });
   });
 
-  it('should get network ip no network', () => {
-    let originalNetworkInterfaces = os.networkInterfaces;
-    try {
-      (<any> os).networkInterfaces = () => { /**/ };
-      test.strictEqual(getNetworkIP(), '');
+  describe('IsLocalHost', () => {
 
-    } catch (e) {
-      test.ok(false, e.message);
-    }
+    const networkInterfacesSpy = sinon.stub().returns({
+        en0: [
+          {
+            address: '10.10.10.10',
+            family: 'IPv4',
+            internal: false
+          }
+        ],
+        utun2: [
+          {
+            address: '10.8.0.9',
+            family: 'IPv4',
+            internal: false
+          }
+        ]
+    });
 
-    (<any> os).networkInterfaces = originalNetworkInterfaces;
+    it('should return isLocalHost', async () => {
+      const { isLocalHost } = createMocks(networkInterfacesSpy);
+      test.ok(await isLocalHost('localhost'));
+      test.ok(await isLocalHost('127.0.0.1'));
+      test.ok(!await isLocalHost('notlocal'));
+      test.ok(!await isLocalHost('1.2.3.4'));
+      test.ok(await isLocalHost('some.host'));
+    });
+
   });
 });
+
+function createMocks(networkInterfacesSpy) {
+  return proxyquire('../src/hostName',
+  {
+    dns: {
+      promises: {
+        lookup: (host) => {
+          if (host === 'some.host') {
+            return Promise.resolve({ address: '10.10.10.10' });
+          }
+          return Promise.reject();
+        }
+      }
+    },
+    os:  {
+      networkInterfaces: networkInterfacesSpy
+    }
+  });
+}
